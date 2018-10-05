c2d476c7c3525e573572831e164fa6bf6ff7cabd
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2018-10-05 20:57:19 +0900

    Add setNewsCompanyList function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import newsSection from './newsSection.js'
 import {fnNewsListTemplate} from '../templates/news.js'
+import { fnNewsCompanyList } from '../templates/company.js'
 
 document.addEventListener("DOMContentLoaded", () => {
     const news = new newsSection();
-    news.init(fnNewsListTemplate);
+    news.init(fnNewsListTemplate, fnNewsCompanyList);
 });
--- js/newsSection.js
+++ js/newsSection.js
@@ -1,8 +1,20 @@
+import { newslist } from '../data/newslist.js'
+
 export default class newsSection {
-    constructor() {
-    }
+  constructor() {
+    this.activeNewsCompanyIndex = 0;
+    this.newsCompanyList = newslist;
+  }
 
-    init(fnNewsListTemplate, fnNewsCompanyList) {
-    }
-
+  init(fnNewsListTemplate, fnNewsCompanyList) {
+    this.setNewsCompanyList(fnNewsCompanyList);
+  }
+  setNewsCompanyList(fnNewsCompanyList) {
+    const newsNavigation = document.querySelector('.newsNavigation');
+    this.newsCompanyList.forEach((news, index) => {
+      const newsCompany = document.createElement('li');
+      newsCompany.innerHTML = fnNewsCompanyList.bind(this)(news);
+      newsNavigation.appendChild(newsCompany);
+    })
+  }
 }
