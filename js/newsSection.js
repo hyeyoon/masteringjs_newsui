2a76663a36ef4d7a1025707ae3551739b515d504
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2018-10-05 21:11:13 +0900

    Add fnNewsListTemplate, setNewsCompanyList function

--- js/newsSection.js
+++ js/newsSection.js
@@ -7,7 +7,13 @@ export default class newsSection {
   }
 
   init(fnNewsListTemplate, fnNewsCompanyList) {
+    this.newsTemplate = fnNewsListTemplate;
+    this.setNewsList();
     this.setNewsCompanyList(fnNewsCompanyList);
+  }
+  setNewsList() {
+    const newsContent = document.querySelector('.content');
+    newsContent.innerHTML = this.newsTemplate(this.newsCompanyList[this.activeNewsCompanyIndex]);
   }
   setNewsCompanyList(fnNewsCompanyList) {
     const newsNavigation = document.querySelector('.newsNavigation');
@@ -17,4 +23,5 @@ export default class newsSection {
       newsNavigation.appendChild(newsCompany);
     })
   }
+  
 }
--- templates/news.js
+++ templates/news.js
@@ -3,18 +3,18 @@ const fnNewsListTemplate = ({logoImgUrl, company, thumbnews:{imageUrl}, thumbnew
     <div class="logo-wrap">
         <img class="company" src="${logoImgUrl}" alt="${company}">
     </div>
-        <button> <a href="#">X</a> </button>
+      <a href="#"><i class="fas fa-times"></i></a>
     </div>
     <div class="newsList">
-        <div class="img-title-wrap">
-            <img src="${imageUrl}" alt="">
-            <div class="title-desc">${text}</div>
-        </div>
-        <ul>
-         ${newslist.reduce((html, newsTitle) => {
-             return html + '<li>' + newsTitle + '</li>';
-         }, "")}
-        </ul>
+      <div class="img-title-wrap">
+        <img src="${imageUrl}" alt="">
+        <div class="title-desc">${text}</div>
+      </div>
+      <ul>
+       ${newslist.reduce((html, newsTitle) => {
+           return html + '<li>' + newsTitle + '</li>';
+       }, "")}
+      </ul>
     </div>`
 }
 
